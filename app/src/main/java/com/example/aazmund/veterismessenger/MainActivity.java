423c93316d4627de0e06ef1fd202176b93a042c9
MadBabCat <AazmundZ@gmail.com>
2017-02-22 16:34:57 +0300

    1

--- app/src/main/java/com/example/aazmund/veterismessenger/MainActivity.java
+++ app/src/main/java/com/example/aazmund/veterismessenger/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.aazmund.veterismessenger;
 
+import android.os.*;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SIGN_IN_REQUEST_CODE = 1;
+    private FirebaseListAdapter<Messenger> adapter;
+    RelativeLayout activity_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activity_main = (RelativeLayout) findViewById(R.id.activity_main);
+
+        //если не зареган то вывести страницу
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_REQUEST_CODE);
+        } else {
+            Snackbar.make(activity_main, "Welcome" + FirebaseAuth.getInstance().getCurrentUser().getEmail(), Snackbar.LENGTH_SHORT).show();
+        }
+        //загрузка контента
+        displayMessage();
     }
+
+    private void displayMessage() {
+        
+    }
+
 }
